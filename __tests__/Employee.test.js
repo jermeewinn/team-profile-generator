53175d56b5122b370d5885b70830309efbcb7160
Jeremy Huynh <jeahuynh18@gmail.com>
2021-11-23 16:06:02 -0800

    added initial testing code for Employee.test.js

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,4 +1,29 @@
-const { test } = require('picomatch');
 const Employee = require('../lib/Employee');
 
-test('')
+
+test('creates an employee name', () => {
+    const employee = 'Dave';
+    const expected = new Employee(employee, '486410', 'dave@ucsc.edu').getName()
+
+    expect(expected).toEqual('Dave');
+});
+
+test('creates an employee ID number', () => {
+    const employee = '48654';
+    const expected = new Employee('Dave', employee, 'dave@ucsc.edu').getId()
+    
+    expect(expected).toEqual('48654');
+    
+});
+
+test('creates an employee email', () => {
+    const employee = 'dave@ucsc.edu';
+    const expected = new Employee('Dave', '46854', employee).getEmail()
+    
+    expect(expected).toEqual('dave@ucsc.edu')
+});
+
+test('creates a role for the employee', () => {
+    
+})
+
