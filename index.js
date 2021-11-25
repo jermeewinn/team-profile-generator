96db04b71785ddcba4684b114701d86a389ea689
Jeremy Huynh <jeahuynh18@gmail.com>
2021-11-24 23:35:27 -0800

    added initial questions for the inquirer.prompt

--- index.js
+++ index.js
@@ -5,3 +5,29 @@ const Employee = require('./lib/Employee');
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
+
+const questions = 
+    ([
+        {
+            type: 'input',
+            name: 'getName',
+            message: "Input your team member's name:"
+        },
+        {
+            type: 'input',
+            name: 'getId',
+            message: "Input your team member's employee ID:"
+        },
+        {
+            type: 'input',
+            name: 'getEmail',
+            message: "Enter your team member's company Email:"
+        },
+        {
+            type: 'checkbox',
+            name: 'getRole',
+            message: "Enter your team memeber's role: (Input Manager, Engineer, or Intern)",
+            choices: ['Manager', 'Engineer', 'Intern']
+        }
+
+    ])
