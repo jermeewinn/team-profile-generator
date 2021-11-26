158ead2b1c172dd1d371cb83bf1a17ce2136d210
Jeremy Huynh <jeahuynh18@gmail.com>
2021-11-26 00:13:49 -0800

    reformatted question logic and enabled user input to save into teamArray

--- index.js
+++ index.js
@@ -6,113 +6,36 @@ const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 
-const questions = 
-    ([
-        // {
-        //     type: 'input',
-        //     name: 'getName',
-        //     message: "Input your team member's name:"
-        // },
-        // {
-        //     type: 'input',
-        //     name: 'getId',
-        //     message: "Input your team member's employee ID:"
-        // },
-        // {
-        //     type: 'input',
-        //     name: 'getEmail',
-        //     message: "Enter your team member's company Email:"
-        // },
-        // {
-        //     type: 'list',
-        //     name: 'getRole',
-        //     message: "Enter your team memeber's role: (Input Manager, Engineer, or Intern)",
-        //     choices: ['Engineer', 'Intern']
-        // },
-        // {
-        //     type: 'input',
-        //     name: 'genCustomInfo',
-        //     message: "If your member is an Engineer, enter their GitHub. If your member is an Intern, enter their school."
-        // },
-        // {
-        //     type: 'list',
-        //     name: 'promptFinish1',
-        //     message: 'Do you have more people on your team?',
-        //     choices: ['Yes', 'No']
-        // }
-    ])
-
 const teamArray = [];
 // Initialized through $node index.js. User is acting as the manager.
-function init() {
-    inquirer.prompt([
-        {
-            type: 'input',
-            name: 'managerName',
-            message: 'Input your First and Last Name:'
-        },
-        {
-            type: 'input',
-            name: 'getOfficeNumber',
-            message: 'Input your company office phone number:'
-        },
-        {
-            type: 'input',
-            name: 'managerID',
-            message: 'Input your employee ID number:'
-        },
-        {
-            type: 'input',
-            name: 'managerEmail',
-            message: 'Input your company email:'
-        },
-        // {
-        //     type: 'list',
-        //     name: 'promptFinish2',
-        //     message: 'Do you have more people on your team?',
-        //     choices: ['Yes', 'No']
-        // }
-    ])   
-        .then(({ role, genCustomInfo }) => {
-            role = 'Manager';
-            team();
-            // if (promptFinish2 === "Yes") {
-            //     newMembers();
-            // } else fs.writeFile('../dist/index.html', template(teamArray), (err) => {
-            //         if (err) {console.log('Try again.', err)}
-            //         console.log("HTML successfully generated!")
-            //     });
-        });  
-};
 
-
-function newMembers() {
+function newMembers(getRole,getCustomInfo) {
     inquirer
         .prompt([
             {
                 type: 'list',
                 name: 'getRole',
-                message: "Enter your team member's role: (Input Manager, Engineer, or Intern)",
-                choices: ['Engineer', 'Intern']
+                message: "Enter your or your team member's role: (Manager, Engineer, or Intern)",
+                choices: ['Manager', 'Engineer', 'Intern']
             },
             {
                 type: 'input',
-                name: 'genCustomInfo',
-                message: "If your member is an Engineer, enter their GitHub. If your member is an Intern, enter their school."
+                name: 'getCustomInfo',
+                message: "If your member is an input manager, please provide the office phone number. If your member is an Engineer, enter their GitHub. If your member is an Intern, enter their school."
             },
             {
                 type: 'input',
-                name: 'getName',
+                name: 'employeeName',
                 message: "Input your team member's name:"
             },
             {
                 type: 'input',
-                name: 'getId',
+                name: 'id',
                 message: "Input your team member's employee ID:"
             },
             {
                 type: 'input',
-                name: 'getEmail',
+                name: 'email',
                 message: "Enter your team member's company Email:"
             },
             {
@@ -122,34 +45,30 @@ function newMembers() {
                 choices: ['Yes', 'No']
             }
         ])
-        .then(({promptFinish1}) => {
-            if (promptFinish1 === "Yes") {
-                return newMembers();
-            }
-        });
+            .then(({getRole,getCustomInfo,employeeName,id,email,promptFinish1}) => {
+                let member;
+                console.log(getRole)
+                if (getRole === 'Engineer') {
+                    member = new Engineer(employeeName, id, email, getCustomInfo)
+                } else if (getRole === 'Intern') {
+                    member = new Intern(employeeName, id, email, getCustomInfo)
+                } else if (getRole === 'Manager') {
+                    member = new Manager(employeeName, id, email, getCustomInfo)
+                }
+                teamArray.push(member);
+                console.log(teamArray)
+                if (promptFinish1 === "Yes") {
+                    newMembers(getRole,getCustomInfo);
+                }
+                else{
+                    fs.writeFile('./dist/index.html', template(teamArray), err => {
+                    if (err) throw err;
+                    console.log("HTML successfully generated!")
+                        }
+                        
+                    )
+                } 
+            })
 }   
-function team(role, genCustomInfo) {
-    newMembers()
-        // if (promptFinish1 === "Yes") {
-        //     return newMembers();
-        // }
-    // inquirer.prompt(questions)
-    //     .then(({ id, employeeName, email, promptFinish1}) => {
-    //         let member;
 
-    //         if (getRole === 'Engineer') {
-    //             member = new Engineer(employeeName, id, email, getCustomInfo)
-    //         } else if (getRole === 'Intern') {
-    //             member = new Intern(employeeName, id, email, getCustomInfo)
-    //         }
-    //         teamArray.push(member);
-        
-    //         if (promptFinish1 === "Yes") {
-    //             newMembers();
-    //         } else fs.writeFile('../dist/index.html', template(teamArray), (err) => {
-    //                 if (err) {console.log('Try again.', err)}
-    //                 console.log("HTML successfully generated!")
-    //             })
-    //     })
-    }
-init();
+newMembers();
